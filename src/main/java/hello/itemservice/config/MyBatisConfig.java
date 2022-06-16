8fee54fd93fcbf95d011f51ff5206d824d4c0fde
jihye.seong <jihye.seong@ipageon.com>
2022-06-16 20:23:22 +0900

    동적 쿼리에 대한 간단한 설명 주석

--- src/main/java/hello/itemservice/config/MyBatisConfig.java
+++ src/main/java/hello/itemservice/config/MyBatisConfig.java
@@ -30,3 +30,30 @@ public class MyBatisConfig {
 
 
 }
+
+/**
+ * ## 동적 SQL
+ *
+ * - *** if ***
+ * - *** choose (when, otherwise) ***
+ * - *** trim (where, set) ***
+ * - *** foreach ***
+ *
+ * choose 는 자바의 switch 구문과 유사하다.
+ *
+ * WHERE 문을 언제 넣어야 할지 상황에 따라서 동적으로 달라지는 문제가 있다.
+ *
+ * 이 때는 <where> 를 사용하면 이런 문제를 해결할 수 있다.
+ *
+ * <where> 는 문장이 없으면 where 를 추가하지 않는다.
+ *
+ * 문장이 있으면 where 를 추가한다.
+ *
+ * 만약 and 가 먼저 시작된다면 and 를 지운다.
+ *
+ * foreach는 컬렉션을 반복 처리할 때 사용한다.
+ *
+ * where in (1,2,3,4,5,6) 와 같은 문장을 쉽게 완성할 수 있다.
+ *
+ * 파라미터로 List 를 전달하면 된다.
+ */
